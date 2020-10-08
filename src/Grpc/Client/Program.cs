6124e9dafe1567af7801897cb9bfe6aa9be452bb
andriyan.krastev <andriyan.krastev@gmail.com>
2020-10-09 00:00:34 +0300

    added grpc server and client

--- src/Grpc/Client/Program.cs
+++ src/Grpc/Client/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Server;
 
 namespace Client
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var response = await client.SayHelloAsync(new HelloRequest { Name = "World", RequestNumber = 1 });
+
+            Console.WriteLine($"Greeting: [{response.Message}] response number: [{response.ResponseNumber}]");
         }
     }
 }
