9ccf6bd863dbf70fd2d641e43c881f8152eb760d
chunk1ty <andriyan.krastev@gmail.com>
2022-05-08 22:47:02 +0300

    Implemented simple cache with absolute expiration

--- src/My/Cache/MyMemoryCache.cs
+++ src/My/Cache/MyMemoryCache.cs
@@ -1,12 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Concurrent;
 
 namespace Cache
 {
-    internal class MyMemoryCache
+    public class MyMemoryCache<TKey, TValue>
     {
+        private class MyCacheEntry<TValue>
+        {
+            public MyCacheEntry(TValue value, long absoluteExpirationTicks)
+            {
+                Value = value;
+                AbsoluteExpirationTicks = absoluteExpirationTicks;
+            }
+
+            public TValue Value { get; }
+
+            public long AbsoluteExpirationTicks { get; }
+        }
+
+        private readonly ConcurrentDictionary<TKey, MyCacheEntry<TValue>> _entries = new ConcurrentDictionary<TKey, MyCacheEntry<TValue>>();
+
+        private readonly TimeSpan _absoluteExpiration;
+
+        public MyMemoryCache(TimeSpan absoluteExpiration)
+        {
+            _absoluteExpiration = absoluteExpiration;
+        }
+
+        public void Set(TKey key, TValue value)
+        {
+            var myCacheEntry = new MyCacheEntry<TValue>(value, DateTime.UtcNow.Ticks);
+            _entries.TryAdd(key, myCacheEntry);
+        }
+
+        public void Remove(TKey key)
+        {
+            _entries.TryRemove(key, out var myCacheEntry);
+        }
+
+        public TValue Get(TKey key)
+        {
+            if (_entries.TryGetValue(key, out MyCacheEntry<TValue> myCacheEntry))
+            {
+                if (CheckExpired(myCacheEntry.AbsoluteExpirationTicks))
+                {
+                    Remove(key);
+                    return default(TValue);
+                }
+
+                return myCacheEntry.Value;
+            }
+
+            return default(TValue);
+        }
+
+        private bool CheckExpired(long absoluteExpirationTicks)
+        {
+            long utcNowTicks = DateTime.UtcNow.Ticks;
+            long cacheEntryAbsoluteExpirationTicks = absoluteExpirationTicks + _absoluteExpiration.Ticks;
+            if (cacheEntryAbsoluteExpirationTicks < utcNowTicks)
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
 }
--- src/My/Cache/Program.cs
+++ src/My/Cache/Program.cs
@@ -1,3 +1,13 @@
-﻿using System;
+﻿using Cache;
+using System;
+using System.Threading.Tasks;
 
-Console.WriteLine("Hello, World! ");
+var myCache = new MyMemoryCache<int, string>(new TimeSpan(0, 0, 50));
+
+myCache.Set(17, "ank");
+
+// await Task.Delay(3000);
+
+var myEntry = myCache.Get(17);
+
+Console.ReadLine();
